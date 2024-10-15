a2c5c5e8cb4c3b4023435cee244ccf412efe15f0
Joaopedro1344 <142310355+Joaopedro1344@users.noreply.github.com>
2024-10-15 08:16:19 -0300

    Add files via upload

--- Java/script.js
+++ Java/script.js
@@ -1,16 +1,21 @@
-const themeToggleBtn= document.getElementById(theme-toggle)
-const body= document.body;
+const themeToggleBtn = document.getElementById('theme-toggle');
+const body = document.body;
+const imagem = document.getElementById('alienista');
 
-    themeToggleBtn.addEventListener('click',function() {
-body.classList.toggle("dark-theme")
+themeToggleBtn.addEventListener('click', function() {
+    body.classList.toggle("dark-theme");
 
-if(body.classList.contains('dark-theme')){
-    themeToggleBtn.textContent= 'Mudar para tema Claro';
-}else{
-    themeToggleBtn.textContent= 'Mudar para Tema Escuro'
-}
-
+    if (body.classList.contains('dark-theme')) {
+        themeToggleBtn.textContent = 'Mudar para tema Claro';
+        imagem.src = 'imagem/alienista.jpg'; 
+    } else {
+        themeToggleBtn.textContent = 'Mudar para Tema Escuro';
+        imagem.src = 'imagem/gravity falss.png'; 
+    }
 });
+
+
+
 
 
 function mudarCor() {
